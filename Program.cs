ab09efd2cdb3a87534112f7f0d489c1e8fee5a12
ironmaiden59 <ralphdcouto1987@gmail.com>
2024-01-10 21:49:30 -0500

    Use if, else, and else if statements to give a prize instead of a win-lose message

--- Program.cs
+++ Program.cs
@@ -14,27 +14,36 @@ int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
   if ((roll1 == roll2) && (roll2 == roll3))
   {
     Console.WriteLine("You rolled triples! +6 bonus to total!");
     total += 6;
   }
-
 else
 {
   Console.WriteLine("You rolled doubles! +2 bonus to total!");
   total += 2;
 }
+Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if(total >= 15){
-  Console.WriteLine("You win!");
+if (total >= 16)
+{
+  Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+  Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+Console.WriteLine("You win a trip for two!");
 }
 else
 {
-  Console.WriteLine("Sorry, you lose.");
+  Console.WriteLine("You win a kitten!");
 }
 
 
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5763dd4bfd31349581b782e0b4766f8a5a6d35da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d22647fafcee25a13a8fda1212889ab4a6f0cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
