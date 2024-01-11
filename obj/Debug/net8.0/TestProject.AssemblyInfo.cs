a9d22647fafcee25a13a8fda1212889ab4a6f0cb
ironmaiden59 <ralphdcouto1987@gmail.com>
2024-01-10 21:38:43 -0500

    Modify the code to remove the stacking bonus for doubles and triples using nesting

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb925b0853f17bc0b227954a9a2bfbf6148452d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5763dd4bfd31349581b782e0b4766f8a5a6d35da")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
